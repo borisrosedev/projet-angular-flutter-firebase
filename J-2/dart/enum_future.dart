f616dae54c7a46ffd584087f88945c2d46b7dfaf
boralex <boris.alexandre.rose@gmail.com>
2023-10-20 11:19:04 +0200

    "feat/J2"

--- J-2/dart/enum_future.dart
+++ J-2/dart/enum_future.dart
@@ -1,3 +1,5 @@
+import '../../J-1/dart/classes/UserAdmin.dart';
+
 enum UserType {
   // c'est un exemple d'enum sophistiqué disons.
   // faites bien attention le dernier cas est suivi d'un ;
@@ -13,4 +15,15 @@ enum UserType {
   final bool isPremium;
 }
 
-void main() {}
+Future<String>? showUserType() {
+  print('and the last word is ...');
+  return Future.delayed(const Duration(seconds: 2), () => 'world ');
+}
+
+Future<void> main() {
+  print('hello');
+  // soit on utilise return puis then mais ensuite on est dans potentiellement un callback hell
+  // soit on utilse async et await
+  return Future.delayed(Duration(milliseconds: 2000))
+      .then((_) async => print(await showUserType()));
+}
